b228ca025524cda2e479f95d6c82e23b2a63605e
Roman-Alenskiy <kuperman88@gmail.com>
2018-12-08 22:09:22 +0300

    Added hourly forecast placeholder

--- src/components/WeatherForecast/HourlyWeather.js
+++ src/components/WeatherForecast/HourlyWeather.js
@@ -6,6 +6,8 @@ import { GridX } from '../Foundation'
 
 import { DAY_IDS } from '../../constants'
 
+import shockedCloud from '../../assets/img/shocked-cloud-icon.svg'
+
 const HourlyWeather = ({ match, list }) => {
   const dayID = DAY_IDS[match.params.dayOfTheWeek]
   let filteredList = []  
@@ -18,26 +20,32 @@ const HourlyWeather = ({ match, list }) => {
   }
 
   return (
-    <GridX className="weather-dock hourly cell auto">
-      {
-        filteredList.map((item) => {
-          const temp = Math.round(item.main.temp)
-          const date = new Date(item.dt * 1000)
-          const formattedDate = date.toTimeString().slice(0, 5)
-          const weather = item.weather[0].main.toLowerCase()
+    <GridX className="weather-dock hourly cell auto align-center align-middle">
+      {filteredList.length !== 0
+        ? filteredList.map((item) => {
+            const temp = Math.round(item.main.temp)
+            const date = new Date(item.dt * 1000)
+            const formattedDate = date.toTimeString().slice(0, 5)
+            const weather = item.weather[0].main.toLowerCase()
 
-          return (
-            <WeatherCardHourly 
-              key={item.dt} 
-              date={formattedDate} 
-              temp={temp} 
-              weather={weather} 
-            />
-          )
-        })
+            return (
+              <WeatherCardHourly 
+                key={item.dt} 
+                date={formattedDate} 
+                temp={temp} 
+                weather={weather} 
+              />
+            )
+          })
+        : <div className="error">
+            <img className="error-icon" src={shockedCloud} alt="error"/>
+            <p>
+              No weather information for today.
+            </p>
+          </div>
       }
     </GridX>
   )
 }
 
-export default HourlyWeather;
+export default HourlyWeather
