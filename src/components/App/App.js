756ef500d1da57849d2b54254d5bae1a89aa594d
Roman Alenskiy <kuperman88@gmail.com>
2019-05-02 20:33:48 +0300

    Now using React function components with hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import axios from 'axios'
-import './App.scss';
+import './App.scss'
 
 import { GridY } from '../Foundation'
 import { DailyWeather, HourlyWeather } from '../WeatherForecast'
@@ -10,120 +10,92 @@ import { Logo } from '../Logo'
 
 import { WEEK_DAYS } from '../../constants'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      weatherForecast: null,
-      citySuggestions: null,
-      locationName: '',
-      isWeatherLoading: false,
-      isCitySuggestionsLoading: false,
-      errorMessage: null,
-      locationSearchCursorCounter: 0,
-    }
+const useOpenWeatherMapAPI = () => {
+  const [weatherForecast, setWeatherForecast] = useState(null)
+  const [citySuggestions, setCitySuggestions] = useState(null)
+  const [locationName, setLocationName] = useState('')
+  const [isWeatherLoading, setIsWeatherLoading] = useState(false)
+  const [isCitySuggestionsLoading, setIsCitySuggestionsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [locationSearchCursorCounter, setLocationSearchCursorCounter] = useState(0)
 
-    this.getUserLocation = this.getUserLocation.bind(this)
-    this.setWeatherForecast = this.setWeatherForecast.bind(this)
-    this.fetchForecastByCoords = this.fetchForecastByCoords.bind(this)
-    this.fetchForecastByName = this.fetchForecastByName.bind(this)
-    this.fetchForecastById = this.fetchForecastById.bind(this)
-    this.fetchCitySuggestions = this.fetchCitySuggestions.bind(this)
-    this.onLocationSearchChange = this.onLocationSearchChange.bind(this)
-    this.onLocationSearchSubmit = this.onLocationSearchSubmit.bind(this)
-    this.onCitySuggestionClick = this.onCitySuggestionClick.bind(this)
-    this.handleLocationSearchNavigation = this.handleLocationSearchNavigation.bind(this)
+  function handleWeatherForecastUpdate(result) {
+    setWeatherForecast(result)
+    setLocationName(`${result.city.name}, ${result.city.country}`)
+    setIsWeatherLoading(false)
+    setErrorMessage(null)
   }
 
-  componentDidMount() {
-    this.getUserLocation()
-  }
-
-  getUserLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({ isWeatherLoading: true })
-      const lat = position.coords.latitude
-      const lon = position.coords.longitude
-      this.fetchForecastByCoords({ lat, lon })
-    })
-  }
-
-  setWeatherForecast(result) {
-    const locationName = `${result.city.name}, ${result.city.country}`
-    this.setState({ weatherForecast: result, locationName, isWeatherLoading: false, errorMessage: null })
-  }
-
-  async fetchForecastByCoords({ lat, lon }) {
+  async function fetchForecastByCoords({ lat, lon }) {
     try {
       const result = await axios(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`)
-      this.setWeatherForecast(result.data)
+      handleWeatherForecastUpdate(result.data)
     } catch (error) {
-      this.setState({ errorMessage: 'Whoops, something went wrong with your geolocation. Try to manually search your city!', isWeatherLoading: false })
+      setErrorMessage('Whoops, something went wrong with your geolocation. Try to manually search your city!')
+      setIsWeatherLoading(false)
     }
   }
 
-  async fetchForecastByName(locationName) {
-    try {
-      const result = await axios(`https://api.openweathermap.org/data/2.5/forecast?q=${locationName}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`)
-      this.setWeatherForecast(result.data)
-    } catch (error) {
-      this.setState({ errorMessage: 'Whoops, city not found...', isWeatherLoading: false })
-    }
-  }
-
-  async fetchForecastById(id) {
+  async function fetchForecastById(id) {
     try {
       const result = await axios(`https://api.openweathermap.org/data/2.5/forecast?id=${id}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`)
-      this.setWeatherForecast(result.data)
+      handleWeatherForecastUpdate(result.data)
     } catch (error) {
-      this.setState({ errorMessage: 'Whoops, city not found...', isWeatherLoading: false })
+      setErrorMessage('Whoops, city not found...')
+      setIsWeatherLoading(false)
     }
   }
 
-  async fetchCitySuggestions(locationName) {
+  async function fetchCitySuggestions(newLocationName) {
     try {
-      const result = await axios(`https://api.openweathermap.org/data/2.5/find?q=${locationName}&APPID=${process.env.REACT_APP_OW_API_KEY}`)
-      this.setState({ citySuggestions: result, isCitySuggestionsLoading: false })
+      const result = await axios(`https://api.openweathermap.org/data/2.5/find?q=${newLocationName}&APPID=${process.env.REACT_APP_OW_API_KEY}`)
+      setCitySuggestions(result)
+      setIsCitySuggestionsLoading(false)
     } catch (error) {
-      this.setState({ citySuggestions: null, isCitySuggestionsLoading: false })
+      setCitySuggestions(null)
+      setIsCitySuggestionsLoading(false)
     }
   }
 
-  onLocationSearchChange(event) {
-    const locationName = event.target.value
+  function onLocationSearchChange(event) {
+    const newLocationName = event.target.value
 
-    this.setState({ locationName: locationName, locationSearchCursorCounter: 0  })
+    setLocationName(newLocationName)
+    setLocationSearchCursorCounter(0)
 
-    if (locationName.length >= 3) {
-      this.setState({ isCitySuggestionsLoading: true })
-      this.fetchCitySuggestions(locationName)
+    if (newLocationName.length >= 3) {
+      setIsCitySuggestionsLoading(true)
+      fetchCitySuggestions(newLocationName)
     } else {
-      this.setState({ citySuggestions: null })
+      setCitySuggestions(null)
     }
   }
 
-  onLocationSearchSubmit(event) {
+  function onLocationSearchSubmit(event) {
     event.preventDefault()
 
     const activeCitySuggestion = document.querySelector('.suggestion.item.active')
     if (!activeCitySuggestion) return
     const cityId = activeCitySuggestion.getAttribute('data-city-id')
 
-    this.setState({ isWeatherLoading: true, citySuggestions: null, locationSearchCursorCounter: 0 })
-    this.fetchForecastById(cityId)
+    setIsWeatherLoading(true)
+    setCitySuggestions(null)
+    setLocationSearchCursorCounter(0)
+
+    fetchForecastById(cityId)
   }
 
-  onCitySuggestionClick(event) {
+  function onCitySuggestionClick(event) {
     const cityId = event.target.getAttribute('data-city-id')
-    
-    this.setState({ isWeatherLoading: true, citySuggestions: null, locationSearchCursorCounter: 0 })
-    this.fetchForecastById(cityId)
+
+    setIsWeatherLoading(true)
+    setCitySuggestions(null)
+    setLocationSearchCursorCounter(0)
+
+    fetchForecastById(cityId)
   }
 
-  handleLocationSearchNavigation(event) {
-
-    const { locationSearchCursorCounter, citySuggestions } = this.state
-
+  function handleLocationSearchNavigation(event) {
     if (!citySuggestions) return
 
     // Preventing cursor from jumping to end and to start of input field
@@ -131,60 +103,110 @@ class App extends Component {
 
     // arrow up/down button should select next/previous list element
     if (event.key === 'ArrowUp' && locationSearchCursorCounter > 0) {
-      this.setState((prevState) => ({
-        locationSearchCursorCounter: prevState.locationSearchCursorCounter - 1
-      }))
+      setLocationSearchCursorCounter(locationSearchCursorCounter - 1)
     } else if (event.key === 'ArrowDown' && locationSearchCursorCounter < citySuggestions.data.list.length - 1) {
-      this.setState((prevState) => ({
-        locationSearchCursorCounter: prevState.locationSearchCursorCounter + 1
-      }))
+      setLocationSearchCursorCounter(locationSearchCursorCounter + 1)
     }
   }
 
-  render() {
-    const { weatherForecast, citySuggestions, locationName, isWeatherLoading, isCitySuggestionsLoading, errorMessage, locationSearchCursorCounter } = this.state
-    let dayList = null
-    let hourList = null
-    let dayOfTheWeek = null
-
-    if (weatherForecast) {
-      dayList = []
-      hourList = weatherForecast.list
-      let days = []
-      
-      // Filter to make a dayList in which each of the days will be included only once
-      dayList = weatherForecast.list.filter((item) => {
-        const itemDate = new Date(item.dt * 1000)
-        if (days.includes(itemDate.getUTCDate())) {return false}
-        days.push(itemDate.getUTCDate())
-        return true
+  useEffect(() => {
+    function getUserLocation() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setIsWeatherLoading(true)
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        fetchForecastByCoords({ lat, lon })
       })
-
-      // Getting current day of the week to appropriate redirect
-      const date = new Date(dayList[0].dt * 1000)
-      dayOfTheWeek = WEEK_DAYS[date.getUTCDay()].toLowerCase()
     }
 
+    getUserLocation()
+  }, [])
 
-
-    return (
-      <GridY className="main grid-container">
-        <Logo />
-
-        <LocationSearch value={locationName} onChange={this.onLocationSearchChange} onSubmit={this.onLocationSearchSubmit} onCitySuggestionClick={this.onCitySuggestionClick} citySuggestions={citySuggestions} isLoading={isCitySuggestionsLoading} cursorCounter={locationSearchCursorCounter} handleNavigation={this.handleLocationSearchNavigation}/> 
-
-        <DailyWeather list={dayList} isLoading={isWeatherLoading} errorMessage={errorMessage}/>
-
-        <Route exact path="/" render={() =>
-          dayOfTheWeek && <Redirect to={`/day/${dayOfTheWeek}`} />} 
-        />
-
-        <Route path="/day/:dayOfTheWeek" render={(props) => 
-          !errorMessage && <HourlyWeather {...props}  list={hourList} />}
-        />
-      </GridY>
-   )
+  return {
+    weatherForecast,
+    isWeatherLoading,
+    errorMessage,
+    locationName,
+    onLocationSearchChange,
+    onLocationSearchSubmit,
+    onCitySuggestionClick,
+    citySuggestions,
+    isCitySuggestionsLoading,
+    locationSearchCursorCounter,
+    handleLocationSearchNavigation,
   }
+}
+
+function App() {
+  const {
+    weatherForecast,
+    isWeatherLoading,
+    errorMessage,
+    locationName,
+    onLocationSearchChange,
+    onLocationSearchSubmit,
+    onCitySuggestionClick,
+    citySuggestions,
+    isCitySuggestionsLoading,
+    locationSearchCursorCounter,
+    handleLocationSearchNavigation,
+  } = useOpenWeatherMapAPI()
+
+  let dayList = null
+  let hourList = null
+  let dayOfTheWeek = null
+
+  if (weatherForecast) {
+    dayList = []
+    hourList = weatherForecast.list
+    const days = []
+
+    // Filter to make a dayList in which each of the days will be included only once
+    dayList = weatherForecast.list.filter((item) => {
+      const itemDate = new Date(item.dt * 1000)
+      if (days.includes(itemDate.getUTCDate())) return false
+      days.push(itemDate.getUTCDate())
+      return true
+    })
+
+    // Getting current day of the week to appropriate redirect
+    const date = new Date(dayList[0].dt * 1000)
+    dayOfTheWeek = WEEK_DAYS[date.getUTCDay()].toLowerCase()
+  }
+
+  return (
+    <GridY className="main grid-container">
+      <Logo />
+
+      <LocationSearch
+        value={locationName}
+        onChange={onLocationSearchChange}
+        onSubmit={onLocationSearchSubmit}
+        onCitySuggestionClick={onCitySuggestionClick}
+        citySuggestions={citySuggestions}
+        isLoading={isCitySuggestionsLoading}
+        cursorCounter={locationSearchCursorCounter}
+        handleNavigation={handleLocationSearchNavigation}
+      />
+
+      <DailyWeather
+        list={dayList}
+        isLoading={isWeatherLoading}
+        errorMessage={errorMessage}
+      />
+
+      <Route
+        exact
+        path="/"
+        render={() => dayOfTheWeek && <Redirect to={`/day/${dayOfTheWeek}`} />}
+      />
+
+      <Route
+        path="/day/:dayOfTheWeek"
+        render={props => !errorMessage && <HourlyWeather {...props} list={hourList} />}
+      />
+    </GridY>
+  )
 }
 
 export default App
--- src/components/LocationSearch/LocationSearch.js
+++ src/components/LocationSearch/LocationSearch.js
@@ -1,77 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react'
 import { DebounceInput } from 'react-debounce-input'
 import './LocationSearch.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { GridX } from '../Foundation'
 
-class LocationSearch extends Component {
-  constructor(props) {
-    super(props)
-    this.searchInput = React.createRef()
-    this.citySuggestion = React.createRef()
+function LocationSearch(props) {
+  const {
+    value, onChange, onSubmit, onCitySuggestionClick, citySuggestions,
+    isLoading, cursorCounter, handleNavigation,
+  } = props
 
-    this.state = {
-      isSuggestionsVisible: false,
-    }
+  const [isSuggestionsVisible, setIsSuggestionsVisible] = useState(false)
 
-    this.onClick = this.onClick.bind(this)
-    this.handleSearchBlur = this.handleSearchBlur.bind(this)
-    this.handleSearchFocus = this.handleSearchFocus.bind(this)
-  }   
+  const searchInput = useRef()
 
-  componentDidMount() {
-    if (this.searchInput.current) this.searchInput.current.focus()
+  function handleSearchBlur() {
+    setIsSuggestionsVisible(false)
   }
 
-  onClick(event) {
+  function handleSearchFocus() {
+    setIsSuggestionsVisible(true)
+  }
+
+  useEffect(() => {
+    if (searchInput.current) { searchInput.current.focus() }
+  }, []) // without second argument focus become permanent https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
+
+  function onClick(event) {
     event.target.select()
   }
 
-  handleSearchBlur() {
-    this.setState({ isSuggestionsVisible: false })
-  }
+  // Rendering
+  let searchSuggestionsClasses = 'search-suggestions menu vertical small-12 medium-6 large-4'
+  if (!isSuggestionsVisible) searchSuggestionsClasses += ' hide'
 
-  handleSearchFocus() {
-    this.setState({ isSuggestionsVisible: true })
-  }
+  let suggestionList
 
-  render() {
-    const { value, onChange, onSubmit, onCitySuggestionClick, citySuggestions, isLoading, cursorCounter, handleNavigation } = this.props
-    const { isSuggestionsVisible } = this.state
+  if (isLoading) {
+    suggestionList = (
+      <li className="suggestion loading">
+        <FontAwesomeIcon className="loading-icon" icon="sync-alt" spin />
+        Loading
+      </li>
+    )
+  } else if (citySuggestions && citySuggestions.data.list.length === 0) {
+    suggestionList = (
+      <li className="suggestion not-found">
+        No suggestion found.
+      </li>
+    )
+  } else if (citySuggestions) {
+    suggestionList = (
+      citySuggestions.data.list.map((item, index) => {
+        const coorMix = `${item.coord.lat}${item.coord.lon}`
+        const { name, id } = item
+        const { country } = item.sys
+        let suggestionClasses = 'suggestion item'
+        suggestionClasses = cursorCounter === index ? suggestionClasses += ' active' : suggestionClasses
 
-    let searchSuggestionsClasses = 'search-suggestions menu vertical small-12 medium-6 large-4'
-    if (!isSuggestionsVisible) searchSuggestionsClasses += ' hide'
-
-    return (
-      <GridX className="location-search cell auto shrink">
-        <form onSubmit={onSubmit} className="search-form cell small-12 medium-6 large-4">
-          <DebounceInput value={value} debounceTimeout="1000" onChange={onChange} onClick={this.onClick} onKeyDown={handleNavigation} onBlur={this.handleSearchBlur} onFocus={this.handleSearchFocus} className="search-input" type="text" inputRef={this.searchInput} />
-          <button className="search-submit" type="submit">
-            <FontAwesomeIcon icon="search" />
-          </button>
-        </form>
-        <ul className={searchSuggestionsClasses}>
-          {isLoading 
-          ? <li className="suggestion loading"><FontAwesomeIcon className="loading-icon" icon="sync-alt" spin/>Loading</li>
-          : citySuggestions && citySuggestions.data.list.length === 0
-            ? <li className="suggestion not-found">No suggestion found.</li>
-            : citySuggestions && citySuggestions.data.list.map((item, index) => {
-                const coorMix = `${item.coord.lat}${item.coord.lon}`
-                const name = item.name
-                const id = item.id
-                const country = item.sys.country
-                let suggestionClasses = 'suggestion item'
-                suggestionClasses = cursorCounter === index ? suggestionClasses += ' active' : suggestionClasses
-
-                return <li key={coorMix} className={suggestionClasses} onMouseDown={onCitySuggestionClick} ref={this.citySuggestion} data-city-id={id}>{`${name}, ${country}`}</li>
-              })
-          }
-        </ul>
-      </GridX>
+        return <li key={coorMix} className={suggestionClasses} onMouseDown={onCitySuggestionClick} data-city-id={id}>{`${name}, ${country}`}</li>
+      })
     )
   }
 
+  return (
+    <GridX className="location-search cell auto shrink">
+      <form onSubmit={onSubmit} className="search-form cell small-12 medium-6 large-4">
+        <DebounceInput value={value} debounceTimeout="1000" onChange={onChange} onClick={onClick} onKeyDown={handleNavigation} onBlur={handleSearchBlur} onFocus={handleSearchFocus} className="search-input" type="text" inputRef={searchInput} />
+        <button className="search-submit" type="submit">
+          <FontAwesomeIcon icon="search" />
+        </button>
+      </form>
+      <ul className={searchSuggestionsClasses}>
+        {suggestionList}
+      </ul>
+    </GridX>
+  )
 }
 
-export default LocationSearch;
+export default LocationSearch
